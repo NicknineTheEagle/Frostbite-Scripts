42a8f3e4adf8c81daf2d8b2666d2df2a879f36ce
CosmicDreamsOfCode <33284629+CosmicDreamsOfCode@users.noreply.github.com>
2021-03-16 18:38:38 +0000

    Update restypes list to match updated layout dumper

--- frostbite3/dumper.py
+++ frostbite3/dumper.py
@@ -13,35 +13,94 @@ from struct import pack,unpack
 #Adjust paths here.
 #do yourself a favor and don't dump into the Users folder (or it might complain about permission)
 
-gameDirectory   = r"D:\Games\OriginGames\Need for Speed(TM) Rivals"
-targetDirectory = r"E:\GameRips\NFS\NFSR\pc\dump"
+gameDirectory   = r"G:\Origin Games\Battlefield 1"
+targetDirectory = r"G:\BF1\DUMP"
 
 #####################################
 #####################################
 
-resTypes={ #not really updated for bf4 though
-    0x5C4954A6:".itexture",
-    0x2D47A5FF:".gfx",
-    0x22FE8AC8:"",
-    0x6BB6D7D2:".streamingstub",
-    0x1CA38E06:"",
-    0x15E1F32E:"",
-    0x4864737B:".hkdestruction",
-    0x91043F65:".hknondestruction",
-    0x51A3C853:".ant",
-    0xD070EED1:".animtrackdata",
-    0x319D8CD0:".ragdoll",
-    0x49B156D4:".mesh",
-    0x30B4A553:".occludermesh",
-    0x5BDFDEFE:".lightingsystem",
-    0x70C5CB3E:".enlighten",
-    0xE156AF73:".probeset",
-    0x7AEFC446:".staticenlighten",
-    0x59CEEB57:".shaderdatabase",
-    0x36F3F2C0:".shaderdb",
-    0x10F0E5A1:".shaderprogramdb",
-    0xC6DBEE07:".mohwspecific",
-    0xafecb022:".luac"
+resTypes = {
+    0xEFC70728: "ZoneStreamerData", # New BF1 Res Asset. Seems to be more of a controling of resourses for loading levels on the fly then a res itself
+    0x86521D6C: ".LMS", # New BF1 "LinearMediaRuntimeResource" Seems to only be used for cinematic SP stuff, maybe mp zep crash
+    0x91043F65: ".HavokPhysicsData",
+    0x4864737B: ".HavokDestructionPhysicsData",
+    0x319D8CD0: ".RagdollResource",
+    0x6BB6D7D2: ".Terrain",
+    0x22FE8AC8: ".TerrainStreamingTree",
+    0x51A3C853: ".AssetBank",
+    0x3568E2B7: ".RawFileData",
+    0xD070EED1: ".AnimationTrackData",
+    0x49B156D4: ".Mesh",  # .MestSet IS WHAT MANY MESH TOOLS USE
+    0x2D47A5FF: ".SwfMovie",  # .swf
+    0x36F3F2C0: ".IShaderDatabase",
+    0xE565EB15: ".DxShaderDatabase",
+    0x10F0E5A1: ".DxShaderProgramDatabase",  # BF4 res
+    0xF04F0C81: ".Dx11ShaderProgramDatabase",
+    0x5C4954A6: ".DxTexture",  # BF4 res
+    0xC417BBD3: ".ITexture",  # BF4 res
+    0x1CA38E06: ".VisualTerrain",
+    0x15E1F32E: ".TerrainDecals",  # BF4 res
+    0x9C4FAA17: ".HeightFieldDecal",
+    0x41D57E10: ".RenderTexture",
+    0x30B4A553: ".OccluderMesh",
+    0x70C5CB3E: ".EnlightenDatabase",
+    0x59CEEB57: ".EnlightenShaderDatabase",
+    0x5BDFDEFE: ".EnlightenSystem",
+    0xE156AF73: ".EnlightenProbeSet",
+    0x7AEFC446: ".StaticEnlightenDatabase",
+    0xC6DBEE07: ".AnimatedPointCloud",
+    0xC78B9D9D: ".ImpulseResponse",
+    0xafecb022: ".Luac",
+    0x957C32B1: ".AtlasTexture",
+    0xC6CD3286: ".EnlightenStaticDatabase",
+    0xA23E75DB: ".TerrainLayerCombinations",
+    0xE36F0D59: ".HavokClothPhysicsData",
+    # IIRC THIS IS WHAT THE "ITEXTURE FILES RES TYPE IS. THIS IS WHERE YOU CAN DECIDE WHAT FILE EXTENSION YOU WANT IT TO BE, EASIER TO DO IT NOW THEN BATCH RENAME EVERYTHING LATER
+    0x6BDE20BA: ".Texture",
+    0x9D00966A:".UITtfFontFile",
+    0x9D00966A: ".TTF",
+    0xC611F34A: ".MeshEmitterResource",
+    0x0DEAFE10: ".IES",  # Illumination Engineering Society, light profiles. google it
+    #0x387CA0AD: '.BNRY'  # MAYBE. bnry is a type of res(texture?) found in The Sims apparently
+    
+    # other resource types
+    0x8D9E6F01: ".PcaComponentWeightsResource",
+    0x85AC783D: ".EAClothAssetData",
+    0xBA02FEE0: ".MeshAdjancencyResource",
+    0x1091C8C5: ".MorphTargetsResource",
+    0x85EA8656: ".EAClothEntityData",
+    0x59C79990: ".FaceFxResource",
+    0xAD1AC4FD: ".LargeParticleCloud",
+    0xEB228507: ".MorphResource",
+    0x52EE0D39: ".PlayerPresetResource",
+    0xAFECB022: ".CompiledLuaResource",
+    0x5E862E05: ".LocalizedStringResource",
+    0xCB8BCD07: ".GtsoLut",
+    0x387CA0AD: ".EAClothData",
+    0xC664A660: ".PamReplayResource",
+    0x76742DC8: ".DelayLoadBundleResource",
+    0x24A019CC: ".MorphMaterialResource",
+    0x428EC9D4: ".BundleRefTableResource",
+    0xEF23407C: ".FifaPhysicsResourceData",
+    0xB2C465F6: ".NewWaveResource",
+    0xB15AD3FD: ".EnlightenShaderDatabaseResource",
+    0x89983F10: ".SvgImage",
+    0x6B4B6E85: ".Dx12PcRvmDatabase",
+    0x50E8E7EE: ".Dx12NvRvmDatabase",
+    0xF7CC814D: ".Dx11NvRvmDatabase",
+    0x8DA16895: ".Dx11RvmDatabase",
+    0x7DD4CC89: ".SerializedExpressionNodeGraph",
+    0x4B803D3B: ".PathfindingRuntimeResource",
+    0x1445F2DB: ".AtlasGroupResource",
+    0x78791C75: ".EmitterGraphResource",
+    0x41759364: ".PhysicsResource",
+    0x00D41D60: ".RaceGroundTextureResource",
+    0x39173AB8: ".MetaMorphTargetMeshResourceAsset",
+    0x59BBF1E8: ".FootballMetaMorphVertexRegionWeightsResource",
+    0x4C4D624A: ".FootballMetaMorphMeshDeltaPositionsResource",
+    0x3B9D1688: ".PSDResource",
+    0x85548684: ".CompressedClipData",
+    0xEC1B7BF4: ".AntResource"
 }
 
 
